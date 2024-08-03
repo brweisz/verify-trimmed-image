8516e58fb7f8eb8701dec51158adbea9215ff9ba
ChinoCribioli <ezequiel.cribioli@gmail.com>
2024-08-02 22:28:17 -0300

    can access image bytes

--- trimmed-image-ui/src/app/server.tsx
+++ trimmed-image-ui/src/app/server.tsx
@@ -1,17 +1,26 @@
 "use server";
 import fs from "node:fs/promises";
 import { revalidatePath } from "next/cache";
+import Jimp from "jimp";
 
 export async function uploadFile(formData: FormData) {
-    console.log("esto es el formData:");
-    console.log(formData);
     const file = formData.get("file") as File;
     const arrayBuffer = await file.arrayBuffer();
     const buffer = new Uint8Array(arrayBuffer);
-    console.log("buffer:");
-    console.log(buffer.length);
+    
+    await fs.writeFile(`./public/uploads/buffer.bmp`, buffer);
 
-    await fs.writeFile(`./public/uploads/buffer`, buffer);
+    const path = "./public/uploads/buffer.bmp"
+    Jimp.read(path)
+        .then((image) => {
+            console.log("imagen:");
+            console.log(image.bitmap.data.buffer.byteLength);
+            console.log(image.bitmap.data[200]);
+        })
+        .catch((err) => {
+            console.log("error");
+            console.log(err);
+        });
 
     revalidatePath("/");
 }

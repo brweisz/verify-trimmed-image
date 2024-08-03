365db697fb62e76eb7ee4ee992d879e175d1199d
ChinoCribioli <ezequiel.cribioli@gmail.com>
2024-08-03 16:07:46 -0300

    retrieving correct offsets

--- trimmed-image-ui/src/app/ImageCropper.tsx
+++ trimmed-image-ui/src/app/ImageCropper.tsx
@@ -38,18 +38,14 @@ const ImageCropper: React.FC<ImageCropperProps> = ({ onOriginalImage, onCroppedI
             const canvas = cropper.getCroppedCanvas();
             const croppedImage = canvas.toDataURL('image/jpeg');
             setCroppedImageSrc(croppedImage)
-            const cropBoxData = cropper.getCropBoxData();
+            const cropBoxData = cropper.getData();
             const originalImageSize = cropper.getImageData();
-
-            // Calculate offsets
-            const offsetX = cropBoxData.left - originalImageSize.left;
-            const offsetY = cropBoxData.top - originalImageSize.top;
 
             // Pass cropped image data, size, and offsets to parent
             onCroppedImage(
                 croppedImage,
-                { width: canvas.width, height: canvas.height },
-                { x: cropBoxData.left, y: cropBoxData.top }
+                { width: cropBoxData.width | 0, height: cropBoxData.height | 0 },
+                { x: cropBoxData.x | 0, y: cropBoxData.y | 0}
             );
         }
     };

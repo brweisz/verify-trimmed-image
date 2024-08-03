014fe0719b5ff5f132f0e522a38602e93feeed1d
ChinoCribioli <ezequiel.cribioli@gmail.com>
2024-08-03 18:37:43 -0300

    found poseidon in typescript

--- trimmed-image-ui/src/app/page.tsx
+++ trimmed-image-ui/src/app/page.tsx
@@ -96,7 +96,17 @@ export function ReaderForm() {
   );
 }
 
+const circomlibjs = require('circomlibjs');
+
+async function test_hash() {
+  const poseidon = await circomlibjs.buildPoseidon();
+  const input = [BigInt(1), BigInt(2), BigInt(3)];
+  const hash = poseidon(input);
+  console.log(hash);
+}
+
 export default function Home() {
+  test_hash();
   return (
     <main className={styles.container}>
       <div className={styles.column}>
